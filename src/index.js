2d2687486597503d8677d7c9b16ddd78d5785078
Alexander <sirbualexandru92@gmail.com>
2019-11-23 17:02:07 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import sum from './js/sum';
+import sum from './js/sum'
 import './styles/style.scss'
-console.log(sum(1,2))
+console.log(sum(1, 2))
+
 console.log('Proof that it works')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=CHISINAU,MD&APPID=7429042c97e6a05117b067c14694f822')
+  .then(response => {
+    return response.json()
+  })
+  .then(result => {
+    console.log(result)
+  })
