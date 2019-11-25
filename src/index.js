e210cad8e9c0c2c7d31e1fdd4465b753687ccd7e
Vladislav Dobrea <vlad.d@goparrot.ai>
2019-11-25 09:50:59 +0200

    [Image select]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import sum from './js/sum';
 import './styles/style.scss';
+import Cities from './js/cities';
+
+console.log(Cities);
+
+const locationSelect = document.getElementById('location-select');
+
+locationSelect.addEventListener('change', (event) => {
+  const city = event.target.value;
+  const defaultCity = 'img/skyline.jpg';
+  const cityImage = document.getElementById('location-image');
+
+  if (!Cities[city]) {
+    cityImage.src = defaultCity;
+    return;
+  }
+
+  cityImage.src = Cities[city].url;
+});
+
 
 console.log(sum(1, 2));
 console.log('Proof that it works');
---
+++ src/js/cities.js
@@ -0,0 +1,24 @@
+const cities = {
+  london: {
+    name: 'London',
+    url: 'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  },
+  chisinau: {
+    url: 'https://sx-content-labs.sixt.io/chisinau-city-header.jpg',
+    name: 'Chisinau',
+  },
+  moscow: {
+    url: 'https://cdn.getyourguide.com/img/tour_img-1233054-98.jpg',
+    name: 'Moscow',
+  },
+  ny: {
+    url: 'https://d12dkjq56sjcos.cloudfront.net/pub/media/wysiwyg/newyork/01-city-landing/New-York-Skyline-Big-Bus-Tours-Jan-2018.jpg',
+    name: 'New York',
+  },
+  paris: {
+    url: 'https://imgix.bustle.com/uploads/shutterstock/2019/9/19/a49124d9-5f62-47a5-b5ec-8dd3a3066b30-shutterstock-1420728554.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70&dpr=2',
+    name: 'Paris',
+  },
+};
+
+export default cities;
