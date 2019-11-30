22638de9ad9a7ff43039420e45271aaaf07207e8
Basarab <sirbualexandru92@gmail.com>
2019-11-30 18:30:36 +0300

    end logical maybe)

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,13 @@
 
-const apiKey = '7429042c97e6a05117b067c14694f822';
+
 
 export function getApi(city){
-   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`)
+   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7429042c97e6a05117b067c14694f822`)
    .then(response => {
     return response.json()
   })
-  
+  .then(result=>{
+      console.log(result)
+  })
 
 }
--- src/js/first.js
+++ src/js/first.js
@@ -6,13 +6,36 @@ export function testing() {
   for (let city in cities) {
     let arrky = [city]
       const block = document.querySelector(".container-main");
+      const block2 = document.querySelector(".wrapper");
       const newItem = document.createElement("div");
-      block.appendChild(newItem);
+      const newP = document.createElement("span")
+block.appendChild(block2)
+      block2.appendChild(newItem);
+      block2.appendChild(newP)
       newItem.classList.add("yo-ho-ho");
-      newItem.innerText = city;
-// console.log(getApi(city))
-  
-  }
-  
+    //   newItem.innerText = city;
+      
+
+        // let exemple = getApi(city)
+
+
+       let x = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7429042c97e6a05117b067c14694f822` 
+       
+
+       fetch(x)
+       .then(response => {
+         return response.json()
+       })
+       .then(result => {
+        console.log(result)
+        const {       
+          main,
+          name      
+        } = result
+        newP.innerText = `${Math.round(main.temp-273.15)}°C`
+        newItem.innerText = name
+     
+    })
+  } 
 }
 testing();
